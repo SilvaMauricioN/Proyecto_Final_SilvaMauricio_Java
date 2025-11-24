cb045accf9e302034f2cd09f7d01f374a1d4f9b2
Silva Mauricio Nicolás <113395913+SilvaMauricioN@users.noreply.github.com>
2025-11-24 19:46:41 -0300

    agregar estructura de carpetas

---
+++ src/main/java/com/museo/api/museo_api/MuseoApiApplication.java
@@ -0,0 +1,13 @@
+package com.museo.api.museo_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuseoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/museo/api/museo_api/MuseoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.museo.api.museo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuseoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
